7df374a0c4aa508514184a66b3be83d9f620392e
Aseel <aseelalnassiry@gmail.com>
2023-04-27 20:58:51 -0700

    add story functionality added

---
+++ js/stories.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// This is the global list of the stories, an instance of StoryList
+let storyList;
+
+/** Get and show stories when site first loads. */
+
+async function getAndShowStoriesOnStart() {
+  storyList = await StoryList.getStories();
+  $storiesLoadingMsg.remove();
+
+  putStoriesOnPage();
+}
+
+/**
+ * A render method to render HTML for an individual Story instance
+ * - story: an instance of Story
+ *
+ * Returns the markup for the story.
+ */
+
+function generateStoryMarkup(story) {
+  // console.debug("generateStoryMarkup", story);
+
+  const hostName = story.getHostName();
+  return $(`
+      <li id="${story.storyId}">
+        <a href="${story.url}" target="a_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <small class="story-author">by ${story.author}</small>
+        <small class="story-user">posted by ${story.username}</small>
+      </li>
+    `);
+}
+
+/** Gets list of stories from server, generates their HTML, and puts on page. */
+
+function putStoriesOnPage() {
+  console.debug('putStoriesOnPage');
+
+  $allStoriesList.empty();
+
+  // loop through all of our stories and generate HTML for them
+  for (let story of storyList.stories) {
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.append($story);
+  }
+
+  $allStoriesList.show();
+}
+
+// Submit handler for stories
+const handleAddStory = async (e) => {
+  e.preventDefault();
+  const author = $('#formAuthor').val();
+  const title = $('#formTitle').val();
+  const url = $('#formUrl').val();
+  const newStory = await storyList.addStory(currentUser, { title, url, author });
+  const $newStory = generateStoryMarkup(newStory);
+  $allStoriesList.prepend($newStory);
+  $addStoryForm.toggleClass('hidden')
+};
+
+$addStoryForm.on('submit', handleAddStory);
